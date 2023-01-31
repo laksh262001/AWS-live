b8902172ccd8ec5e6fa7ad36c6a8d8f4ef64b83d
laksh262001 <t.s.lakshmi2@gmail.com>
2023-01-31 21:51:33 +0530

    First commit

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c3bazma86gaa.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Lakshmi26"
+customdb = "employee"
+custombucket = "addemployee26"
+customregion = "us-east-1"
+
